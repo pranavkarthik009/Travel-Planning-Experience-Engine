529490cb2809c02e254578ed62030b3bf96e3c21
pranavkarthik009 <mvpranavkarthik.gmail.com>
2026-05-08 12:46:42 +0530

    Fix Express 5 catch-all route wildcard bug

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ io.on('connection', (socket) => {
 });
 
 // For any other requests, send back index.html
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
